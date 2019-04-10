d34e5d5b31a15288dde87be00e4b8a8d34250f34
Manny <coachmannytw@gmail.com>
2019-04-10 08:10:55 -0600

    updated user.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,68 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
-router.get('/test', (req, res) => res.json({msg: 'users works'}))
+const User = require("../../models/User");
 
-module.exports = router
+router.get("/test", (req, res) => res.json({ msg: "users works" }));
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: 200, //Size
+        rating: "pg", //Rating
+        d: "mm" //Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .then(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+//API Login
+//Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find by email
+  User.findOne({ email })
+  .then(user => {
+    //check for user
+    if (!user) {
+      return res.status(400).json({ email: "User not found" });
+    }
+    //check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+        if(isMatch) {
+            res.json({msg: 'Success'})
+        } else {
+      return res.status(400).json({msg: "Not a success"})
+     }
+    
+    
+  });
+});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
@@ -7,6 +9,10 @@ const posts = require('./routes/api/posts')
 
 
 const app = express();
+
+//bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //DB Config
 const db = require('./config/keys').mongoURI;
